7c07796bb3b103da6730c2925baa90e059473da2
abagariya4-netizen <abagariya4@gmail.com>
2026-06-17 17:54:08 +0530

    Revert "Fix bug: restore excluded keywords for accurate Unknown and Rest calculation"
    This reverts commit ddb614a85914436d7c02f16ae6b8dc234e4950d5.

--- test-smg.ts
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-async function test() {
-  // We can't easily fetch Google Ads without the proper tokens here in a node script since it relies on next.js env.
-  // I will just use the next.js API by adding a temporary debug param to the route!
-}
